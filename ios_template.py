eb8fa791454b80702fa4ee0b9b813abd535e5b8e
Trong Dinh <trongdth@gmail.com>
2015-09-22 23:58:58 +0700

    add py code

---
+++ ios_template.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import getopt
+import subprocess
+import re
+
+__author__ = 'Trong Dinh (trongdth@gmail.com)'
+__ERR1__ = 'Missing path to template folder'
+__ERR2__ = 'There is an error with your Xcode!!!'
+__xcode4_project_template_path = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/Xcode/Templates/Project Templates"
+__xcode6_project_template_path = "/Users/" + subprocess.check_output("echo $USER", shell=True).rstrip() + "/Library/Developer/Xcode/Templates/Project Templates"
+__sudo_pwd = "(YOUR_PASSWORD_HERE)"
+
+
+def go_to_template_folder(xcode_template):
+    root_template = "Mroom\ Software"
+    try:
+        os.chdir(xcode_template)
+        os.system("echo " + __sudo_pwd + " | sudo -S mkdir -p " + root_template)
+        os.chdir(xcode_template + "/" + root_template)
+    except OSError:
+        print __ERR2__
+        exit()
+
+
+def copy_template(path):
+    value = subprocess.check_output(["xcodebuild", "-version"])
+    xcode_version = re.search("\\d.\\d", value)
+    if xcode_version.group() < 6.0:
+        go_to_template_folder(__xcode4_project_template_path)
+
+    go_to_template_folder(__xcode6_project_template_path)
+    os.system("echo " + __sudo_pwd + " | sudo -S cp -R " + path + " .")
+
+
+def main(argv):
+    path_to_template = ''
+    try:
+        opts, args = getopt.getopt(argv, "hd:", ["dTemplatePath="])
+    except getopt.GetoptError:
+        print __ERR1__
+        sys.exit()
+    for opt, arg in opts:
+        if opt == '-h':
+            print "python ios_template.py -d {path_to_your_template}"
+            sys.exit()
+        elif opt in ("-d", "--dTemplatePath"):
+            path_to_template = arg
+
+    if len(path_to_template) == 0 | len(opts) != 1:
+        print __ERR1__
+        exit()
+    copy_template(path_to_template)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
